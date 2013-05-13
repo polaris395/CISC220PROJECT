ac412820952c9368a4d35711d1e36c281ef3e378
polaris395 <mitcdenn@udel.edu>
2013-05-12 21:17:17 -0300

    Update Project.cpp
    Updated the wrong file.

--- CISC220PROJECT/Project.cpp
+++ CISC220PROJECT/Project.cpp
@@ -1,271 +1,116 @@
-<<<<<<< HEAD
-#include <cmath>
-#include <ctime>
-#include <cerrno>
-#include <csignal>
-#include <cstring>
-#include <cstdlib>
-
-#include <iostream>
-#include <fstream>
-#include <iomanip>
-#include <utility>
-
-#include "Network.h"
-#include "Dijkstra.h"
 #include "Project.h"
-
-using namespace std;
-
-class Scenario {
-public:
-  vector<double> funding;
-  double accumulatedFunding;
-  Project project;
-  int year;
-  double totalDistance;
-  const char * outputFile;
-
-  void initialize(const char * networkFileName,
-      const char * routesFileName, const char * fundingFileName) {
-    totalDistance = 0;
-    year = 0;
-    outputFile = "out";
-    accumulatedFunding = 0;
-
-    project.initialize(networkFileName, routesFileName);
-    ifstream inFile(fundingFileName);
-    if (!inFile) {
-      cerr << "Funding file could not be opened" << endl;
-    }
-    else {
-      while (!inFile.eof()) {
-        double value = -1;
-        inFile >> value;
-        inFile.ignore(1024,'\n');
-        if (value >= 0) {
-          funding.push_back(value);
-        }
-      }
-    }
-  }
-
-  bool nextYear() {
-      double before = project.network.getTotalTrackDistance();
-      accumulatedFunding += funding[year];
-
-      // cheating protection here, create a map of link pairings
-      //  and make sure they didn't remove any
-      map<pair<Station*,Station*>,bool> knownLinks;
-
-      for (vector<Station*>::iterator iter = project.network.vertices.begin();
-               iter != project.network.vertices.end(); iter++) {
-          Station* v = *iter;
-
-          for (vector<Link*>::iterator linkIter = v->links.begin();
-              linkIter != v->links.end(); linkIter++) {
-            Link* l = *linkIter;
-            knownLinks.insert(make_pair(make_pair(l->source,l->target),true));
-          }
-      }
-
-      project.addLinks(accumulatedFunding);
-
-      for (vector<Station*>::iterator iter = project.network.vertices.begin();
-               iter != project.network.vertices.end(); iter++) {
-          Station* v = *iter;
-
-          for (vector<Link*>::iterator linkIter = v->links.begin();
-              linkIter != v->links.end(); linkIter++) {
-            Link* l = *linkIter;
-            knownLinks.erase(make_pair(l->source,l->target));
-          }
-      }
-
-      if (knownLinks.size() > 0) {
-        cerr << "Existing links not allowed to be removed!" << endl;
-        exit(1);
-      }
-
-      double after = project.network.getTotalTrackDistance();
-      if (after - before > accumulatedFunding) {
-        cerr << "Too much funding used to build new track!" << endl;
-        exit(1);
-      }
-      else {
-        accumulatedFunding -= (after - before);
-      }
-      char newoutputFile[1024];
-      sprintf(newoutputFile,"%s_%d.txt",outputFile,year);
-
-      project.network.writeToFile(newoutputFile);
-
-
-      double totalDistanceYear = project.totalDistanceTraveled();
-      totalDistance += totalDistanceYear;
-      cout << "Year "<< year << " Total Distance Traveled=" << totalDistanceYear << " (" << totalDistance << ")" << endl;
-
-      year++;
-
-      return year < funding.size();
-  }
-};
-
-int main(int argc, char **argv) {
-  const char * networkname = "cities.txt";
-  const char * schedulename = "routes.txt";
-  const char * fundingname = "shortterm_funding.txt";
-  if (argc > 1) {
-    networkname = argv[1];
-  }
-  if (argc > 2) {
-    schedulename = argv[2];
-  }
-  if (argc > 3) {
-    fundingname = argv[3];
-  }
-
-  Scenario s;
-  s.initialize(networkname, schedulename, fundingname);
-
-  while (s.nextYear()) {};
-  cout << "Funding left over=" << s.accumulatedFunding << endl;
-  cout << "Sum of Total Distance Traveled=" << s.totalDistance << endl;
-  return 0;
-}
-=======
-#include <cmath>
-#include <ctime>
-#include <cerrno>
-#include <csignal>
-#include <cstring>
-#include <cstdlib>
-
-#include <iostream>
-#include <fstream>
-#include <iomanip>
-#include <utility>
-
-#include "Network.h"
 #include "Dijkstra.h"
-#include "Project.h"
+#include <list>
 
-using namespace std;
-
-class Scenario {
-public:
-  vector<double> funding;
-  double accumulatedFunding;
-  Project project;
-  int year;
-  double totalDistance;
-  const char * outputFile;
-
-  void initialize(const char * networkFileName,
-      const char * routesFileName, const char * fundingFileName) {
-    totalDistance = 0;
-    year = 0;
-    outputFile = "out";
-    accumulatedFunding = 0;
-
-    project.initialize(networkFileName, routesFileName);
-    ifstream inFile(fundingFileName);
-    if (!inFile) {
-      cerr << "Funding file could not be opened" << endl;
-    }
-    else {
-      while (!inFile.eof()) {
-        double value = -1;
-        inFile >> value;
-        inFile.ignore(1024,'\n');
-        if (value >= 0) {
-          funding.push_back(value);
-        }
-      }
-    }
-  }
-
-  bool nextYear() {
-      double before = project.network.getTotalTrackDistance();
-      accumulatedFunding += funding[year];
-
-      // cheating protection here, create a map of link pairings
-      //  and make sure they didn't remove any
-      map<pair<Station*,Station*>,bool> knownLinks;
-
-      for (vector<Station*>::iterator iter = project.network.vertices.begin();
-               iter != project.network.vertices.end(); iter++) {
-          Station* v = *iter;
-
-          for (vector<Link*>::iterator linkIter = v->links.begin();
-              linkIter != v->links.end(); linkIter++) {
-            Link* l = *linkIter;
-            knownLinks.insert(make_pair(make_pair(l->source,l->target),true));
-          }
-      }
-
-      project.addLinks(accumulatedFunding);
-
-      for (vector<Station*>::iterator iter = project.network.vertices.begin();
-               iter != project.network.vertices.end(); iter++) {
-          Station* v = *iter;
-
-          for (vector<Link*>::iterator linkIter = v->links.begin();
-              linkIter != v->links.end(); linkIter++) {
-            Link* l = *linkIter;
-            knownLinks.erase(make_pair(l->source,l->target));
-          }
-      }
-
-      if (knownLinks.size() > 0) {
-        cerr << "Existing links not allowed to be removed!" << endl;
-        exit(1);
-      }
-
-      double after = project.network.getTotalTrackDistance();
-      if (after - before > accumulatedFunding) {
-        cerr << "Too much funding used to build new track!" << endl;
-        exit(1);
-      }
-      else {
-        accumulatedFunding -= (after - before);
-      }
-      char newoutputFile[1024];
-      sprintf(newoutputFile,"%s_%d.txt",outputFile,year);
-
-      project.network.writeToFile(newoutputFile);
-
-
-      double totalDistanceYear = project.totalDistanceTraveled();
-      totalDistance += totalDistanceYear;
-      cout << "Year "<< year << " Total Distance Traveled=" << totalDistanceYear << " (" << totalDistance << ")" << endl;
-
-      year++;
-
-      return year < funding.size();
-  }
+struct PotentialLinks {
+  RouteSchedule * route;
+	Station * source;
 };
+void findCloserLinks(std::vector<Station*> *stationvector, Station* source, Station* destination, int parent);
 
-int main(int argc, char **argv) {
-  const char * networkname = "cities.txt";
-  const char * schedulename = "routes.txt";
-  const char * fundingname = "shortterm_funding.txt";
-  if (argc > 1) {
-    networkname = argv[1];
+double Project::totalDistanceTraveled() {
+  //PSEUDO-CODE
+  // your implementation goes here
+  //Create a Table
+  //double sum = 0;
+  //Get the rail schedule from 'this'
+  //For each route in rail schedule:
+	//Check if we need to calculate DJA
+	//If yes:
+	  //Run the algorithm (network, source of the current route schedule, Table)
+	//Get min distance from Table for destination in route schedule destination
+	//value = route schedule passengers * min distance
+	//sum += value
+  //return sum
+  map<Station *, StationRecord *> table;
+  double sum = 0;
+  RouteSchedule * currRouteSchedule;
+  Station * currStation = NULL;
+  for (vector<RouteSchedule*>::iterator iter = railSchedule.routeSchedules.begin();
+				iter != railSchedule.routeSchedules.end(); ++iter) {
+	currRouteSchedule = *iter;
+	if (currStation == NULL || currRouteSchedule->source->id != currStation->id) {
+		currStation = currRouteSchedule->source;
+		dijkstra_getShortestPaths(&network, currStation, table);
+	}
+	sum += table[currRouteSchedule->destination]->dist * (double)currRouteSchedule->passengers;
   }
-  if (argc > 2) {
-    schedulename = argv[2];
-  }
-  if (argc > 3) {
-    fundingname = argv[3];
-  }
-
-  Scenario s;
-  s.initialize(networkname, schedulename, fundingname);
-
-  while (s.nextYear()) {};
-  cout << "Funding left over=" << s.accumulatedFunding << endl;
-  cout << "Sum of Total Distance Traveled=" << s.totalDistance << endl;
-  return 0;
+  return sum;
 }
->>>>>>> branch 'master' of https://github.com/polaris395/CISC220PROJECT.git
+
+void Project::addLinks(double funding){
+//	Station * two;
+//	Station * three;
+//	for (vector<Station*>::iterator iter = network.vertices.begin();
+//			iter != network.vertices.end(); ++iter) {
+//		Station * stat = *iter;
+//		if (stat->id == 2){
+//			two = stat;
+//		}
+//		if (stat->id == 3){
+//			three = stat;
+//		}
+//	}
+//	addLink(two, three);
+
+	RouteSchedule * currRouteSchedule;
+	Station * currStation = NULL;
+	map<double,PotentialLinks*> linkmap;
+	list<double> usefullist;
+	map<RouteSchedule*, bool> usedroutes;
+	for (vector<RouteSchedule*>::iterator iter = railSchedule.routeSchedules.begin();
+		iter != railSchedule.routeSchedules.end(); ++iter) {
+		currRouteSchedule = *iter;
+		usedroutes[currRouteSchedule] = false;
+		std::vector<Station*>* stations = new vector<Station*>;
+		map<Station *, StationRecord *> table;
+		findCloserLinks(stations, currRouteSchedule->source, currRouteSchedule->destination,0); //Include above?
+		dijkstra_getShortestPaths(&network, currRouteSchedule->source, table); // Optimize to only run when required
+
+		for (vector<Station*>::iterator iter2 = stations->begin(); iter2 != stations->end(); ++iter2) {
+			Station * current_station = * iter2;
+			double usefulness = ((table[current_station]->dist + current_station->getDistance(currRouteSchedule->destination)) -
+					table[currRouteSchedule->destination]->dist)*currRouteSchedule->passengers /
+							current_station->getDistance(currRouteSchedule->destination);
+			usefullist.push_back(usefulness);
+			PotentialLinks * tempLink= new PotentialLinks; //need to delete
+			tempLink->source = current_station;
+			tempLink->route = currRouteSchedule;
+			linkmap[usefulness] = tempLink;
+		}
+	}
+	usefullist.sort();
+	bool stop = false;
+	for (list<double>::iterator iter3 = usefullist.begin(); iter3 != usefullist.end(); ++iter3){
+		double usefulvalue = *iter3;
+		if (!usedroutes[linkmap[usefulvalue]->route]){
+			if (linkmap[usefulvalue]->source->getDistance(linkmap[usefulvalue]->route->destination) <= funding){
+				//for (vector<Link*>::iterator iter4 = linkmap[usefulvalue]->route->destination->links.begin(); iter3 != usefullist.end(); ++iter3){
+				addLink(linkmap[usefulvalue]->source, linkmap[usefulvalue]->route->destination);
+				usedroutes[linkmap[usefulvalue]->route] = true;
+				funding -= linkmap[usefulvalue]->source->getDistance(linkmap[usefulvalue]->route->destination);
+			}
+		}
+
+	}
+}
+
+void findCloserLinks(std::vector<Station*> *stationvector, Station* source, Station* destination, int parent){
+	int sourcedist = source->getDistance(destination);
+	stationvector->push_back(source);
+	parent = stationvector->size() - 1;
+	for (vector<Link*>::iterator iter2 = source->links.begin(); iter2 != source->links.end(); ++iter2){
+		Link * link = *iter2;
+		if (link->target->id == destination->id){
+			stationvector->erase(stationvector->begin()+parent, stationvector->end());
+			break;
+		}
+		else {
+			Station * target = link->target;
+			if (target->getDistance(destination) < sourcedist){
+				//stationvector.push_back(target);
+				findCloserLinks(stationvector, target, destination, parent);
+			}
+		}
+	}
+}
